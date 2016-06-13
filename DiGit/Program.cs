2d39b5e9bb489dacf871780fdc6153ad3e34b675
tbener <tal.bener@allscripts.com>
2016-06-13 11:37:32 +0300

    Start checking Digit server - doesn't work!!! push because of formatting computer

--- DiGit/Program.cs
+++ DiGit/Program.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using DiGit.Commands;
 using DiGit.Helpers;
+using DiGit.Model;
 using DiGit.View;
 
 namespace DiGit
@@ -13,6 +14,10 @@ namespace DiGit
     /// <summary>
     /// This class was written with the help of these links:
     /// http://blog.clauskonrad.net/2010/08/wpf-where-is-main-method-or-how-to.html
+    /// 1) Select your project’s current startup file (app.xaml)
+    /// 2) Set BuildAction = Page
+    /// 3) Add a file as below (Program.cs)
+    /// Note: Do not call this file Application.cs and the class Application. It will conflict with the inner workings of WPF
     /// http://blog.clauskonrad.net/2011/04/wpf-how-to-make-your-application-single.html (forgot to release)
     /// http://stackoverflow.com/questions/19147/what-is-the-correct-way-to-create-a-single-instance-application
     /// </summary>
@@ -21,6 +26,11 @@ namespace DiGit
         [STAThread]
         public static void Main(string[] args)
         {
+            UserStat us = new UserStat();
+            us.Activities.Add(new UserStat.ActivityInfoClass(){ActivityType = UserStat.ActivityType.Startup, Date = DateTime.Now});
+            us.Update();
+            return;
+
             string step = "Init Mutex";
             Mutex mutex = null;
             try
