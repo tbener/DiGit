971fff9c3ca5dd28905419ccfa61c9263bf154e1
Tal Bener <tbener@hotmail.com>
2016-06-20 00:20:40 +0300

    Update NotifyIconWpf project and add some new files (first commit after formatting computer - working)

---
+++ DiGit/ViewModel/PathClassViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DiGit.Commands;
+using DiGit.Helpers;
+using DiGit.Model;
+
+namespace DiGit.ViewModel
+{
+    public class PathClassViewModel
+    {
+        public ICommand SetFavorite { get; set; }
+
+        public PathClassViewModel(PathClass pathClass)
+        {
+            this.PathClass = pathClass;
+            SetFavorite = new RelayCommand(() => Msg.Show("Hello test"));
+
+        }
+
+        public PathClass PathClass { get; set; }
+    }
+}
---
+++ SchedulerK/IEventClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SchedulerK
+{
+
+    public delegate void DeletedEventHandler(IEventClass evt);
+    public delegate void ActivatedEventHandler(IEventClass evt);
+    public delegate void EventTimeChangedEventHandler(IEventClass evt);
+
+    public interface IEventClass : IComparable
+    {
+        event DeletedEventHandler OnDeleted;
+        event ActivatedEventHandler OnActivated;
+        event EventTimeChangedEventHandler OnTimeChanged;
+
+        object Tag { get; set; }
+
+        /// <summary>
+        /// Get or set the description of the event
+        /// </summary>
+        string Description { get; set; }
+
+        /// <summary>
+        /// Returns the destination time of this event
+        /// </summary>
+        DateTime DateTimeEvent { get; }
+        TimeSpan Interval { get; set; }
+
+        /// <summary>
+        /// This method is called by Scheduler object to raise a specific event for
+        /// the current event class.
+        /// </summary>
+        void Activate();
+
+        int EventId { get; set; }
+
+        void Compute();
+
+        bool IsDone { get; set; }
+    }
+}
