5e2fe77e00cb74e509aa293c156adedc11445ef3
Tal Bener <tbener@hotmail.com>
2016-10-31 15:12:51 +0200

    Add settings files (still not in use)

---
+++ DiGit/ViewModel/Settings/BubblesViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiGit.Model;
+using DiGit.Configuration;
+using System.Collections.ObjectModel;
+using DiGit.Helpers;
+
+namespace DiGit.ViewModel.Settings
+{
+    public class BubblesViewModel : BaseSettingsViewModel
+    {
+        //private ObservableCollection<DiGitConfigRepository> RepositoryList;
+
+        public BubblesViewModel()
+        {
+            RepositoryList = new ObservableCollection<DiGitConfigRepository>(ConfigurationHelper.Configuration.RepositoryList);
+            //RepositoriesManager.Repos[0].
+        }
+
+        public ObservableCollection<DiGitConfigRepository> RepositoryList { get; set; }
+      
+        
+
+        public bool AttachToRight
+        {
+            get
+            {
+                return BubblesManager.Anchor == System.Windows.HorizontalAlignment.Right;
+            }
+            set
+            {
+                BubblesManager.Anchor = value ? System.Windows.HorizontalAlignment.Right : System.Windows.HorizontalAlignment.Left;
+            }
+        }
+    }
+}
